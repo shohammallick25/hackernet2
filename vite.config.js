c2a0609aab79715461694d91d4e766aff90b594a
shohammallick25 <shohammallick528@gmail.com>
2026-04-08 03:52:43 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/hackernet2/'  // <-- Add this line for GitHub Pages
+  plugins: [react()]
 })
